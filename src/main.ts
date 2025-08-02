8b397f52f338c18f9486059f3e2522443f3bace9
Robinson Castro F <r.a.c.f.28@gmail.com>
2025-08-02 17:25:12 -0400

    get 4 random cards

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,37 @@
 import Phaser from "phaser";
+import {generateDeck, getCardKey, shuffle} from "./model.ts";
 
 const gameWidth = window.innerWidth;
 const gameHeight = window.innerHeight;
 const cardHeight = gameHeight / 3;
 const cardWidth = cardHeight / Math.sqrt(2);
 const gapX = gameWidth / 80;
+
+let deck = generateDeck();
 new Phaser.Game({
     type: Phaser.AUTO,
     width: gameWidth,
     height: gameHeight,
     scene: {
         preload() {
-            this.load.svg("1B", "cards/1B.svg", { width: cardWidth, height: cardHeight });
+            for (const card of deck) {
+                const key = getCardKey(card);
+                this.load.svg(key, `cards/${key}.svg`, { width: cardWidth, height: cardHeight });
+            }
         },
         create() {
-            const n = 4;
-            for (let i = 0; i < n; i++) {
+            shuffle(deck);
+
+            const dayCards = deck.slice(0, 4);
+            for (let i = 0; i < dayCards.length; i++) {
                 this.add.image(
-                    gameWidth / 2 + (i - (n - 1) / 2) * (cardWidth + gapX),
+                    gameWidth / 2 + (i - (dayCards.length - 1) / 2) * (cardWidth + gapX),
                     gameHeight / 2,
-                    "1B",
+                    getCardKey(dayCards[i]),
                 );
             }
         },
         update() {},
     }
 });
+
---
+++ src/model.ts
@@ -0,0 +1,30 @@
+type Card = {
+    /** A = 1, J = 11, Q = 12, K = 13 */
+    value: number;
+    suit: Suit;
+}
+
+enum Suit {
+    Clubs = "Clubs",
+    Diamonds = "Diamonds",
+    Hearts = "Hearts",
+    Spades = "Spades",
+}
+
+export function shuffle<T>(arr: T[]): void {
+    arr.sort(() => Math.random() - 0.5);
+}
+
+export function generateDeck(): Card[] {
+    let cards: Card[] = [];
+    for (let value = 1; value <= 13; value++) {
+        for (const suit of Object.values(Suit)) {
+            cards.push({ value, suit });
+        }
+    }
+    return cards;
+}
+
+export function getCardKey(card: Card): string {
+    return "0A23456789TJQK"[card.value] + card.suit[0];
+}
