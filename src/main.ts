d3fa0f7a6b432445834995e54e29716eaa4bfb66
Robinson Castro F <r.a.c.f.28@gmail.com>
2025-08-02 16:53:23 -0400

    show 4 card backs

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import Phaser from "phaser";
 
+const cardWidth = 150;
+const cardHeight = 210;
+const gapX = 10;
 new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     scene: {
         preload() {
-            this.load.svg("1B", "cards/1B.svg");
+            this.load.svg("1B", "cards/1B.svg", { width: cardWidth, height: cardHeight });
         },
         create() {
-            this.add.image(400, 300, "1B");
+            for (let i = 0; i < 4; i++) {
+                this.add.image(400 + (i - 1.5) * (cardWidth + gapX), 300, "1B");
+            }
         },
         update() {},
     }
